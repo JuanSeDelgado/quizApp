a3d0eea12fc3987458adc34fcc2ebbb098920f48
JuanSeDelgado <73569662+JuanSeDelgado@users.noreply.github.com>
2022-03-28 11:50:51 -0500

    Avance Js

--- src/js/game.js
+++ src/js/game.js
@@ -8,7 +8,7 @@ let currentQuestion ={};
 let acceptingAnswers= true;
 let score = 0 ;
 let questionCounter = 0;
-let availableQuestion=[];
+let availableQuestions=[];
 
 let questions=[
     {
@@ -44,3 +44,35 @@ let questions=[
         answer:2,
     }
 ]
+
+const SC0RE_POINTS = 100;
+const MAX_QUESTIONS = 4;
+
+startGame = () =>{
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion()
+}
+
+getNewQuestion = () =>{
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
+
+        return window.location('/end.html');
+    }
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS)*100}%`;
+
+    const questionsIndex = Math.floor(Math.random()*availableQuestions.length);
+    currentQuestion = availableQuestions[questionsIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice'+ number];
+    })
+
+
+}
